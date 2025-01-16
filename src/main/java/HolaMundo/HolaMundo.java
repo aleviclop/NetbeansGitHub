21fe60426b48b9516a7dc695ea867a20e1979167
Alex Vicente Lopez <aleviclop2@alu.edu.gva.es>
2025-01-16 18:05:25 +0100

    Añadido mi nombre

---
+++ src/main/java/HolaMundo/HolaMundo.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package HolaMundo;
+
+/**
+ *
+ * @author Alex
+ */
+public class HolaMundo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Mi nombre es Alex");
+    }
+}
